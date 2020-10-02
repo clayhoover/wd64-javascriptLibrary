de8324da561f54c10281fcbbd857029de39bc454
Clay Hoover <ClayHoover@Clays-MBP.attlocal.net>
2020-10-02 10:53:54 -0400

    10/2

---
+++ Challenges/Repl's/Guessing Game.js
@@ -0,0 +1,31 @@
+alert("You have 3 guesses. Guess between 1-10")
+
+var answer = 3;
+var guess;
+
+for( i = 0; i <= 10; i++){
+    guess = prompt("What's your guess?");
+    if (answer == guess) {
+        alert("You got it right!")
+        break;
+    } else {
+        alert("Try again")
+    }
+    }
+}
+
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 3;
+let resetButton;
+
+function checkGuess() {
+    alert('I am a placeholder');
+  }
